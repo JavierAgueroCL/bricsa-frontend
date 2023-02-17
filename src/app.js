2813b0a217217bd13d1db4ae02a820838e784792
Javier Aguero <javieraguerocl@gmail.com>
2023-02-17 01:55:14 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './stylesheet/style.scss';
+//import 'bootstrap';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+require('./plugins/modernizr-3.11.2.min');
+require('./scripts/script');
---
+++ src/scripts/script.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$(() => {
+    $('#nav-icon1').click(function(){
+      $(this).toggleClass('open');
+    });
+});
